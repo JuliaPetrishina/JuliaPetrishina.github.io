d212fdcaf66eb6fb9dad543e267f060db66427be
Julia-Petrishina <julia.petrishina@teamdev.com>
2016-08-20 02:58:09 +0300

    Add filter for sorted table and implementation on prototypes

---
+++ app/pages/works/sortTable-prototype/js/helpers.js
@@ -0,0 +1,64 @@
+
+window.extend = function () {
+
+    // Variables
+    var extended = {};
+    var deep = false;
+    var i = 0;
+    var length = arguments.length;
+
+    // Check if a deep merge
+    if ( Object.prototype.toString.call( arguments[0] ) === '[object Boolean]' ) {
+        deep = arguments[0];
+        i++;
+    }
+
+    var merge = function (obj) {
+        for ( var prop in obj ) {
+            if ( Object.prototype.hasOwnProperty.call( obj, prop ) ) {
+                // If deep merge and property is an object, merge properties
+                if ( deep && Object.prototype.toString.call(obj[prop]) === '[object Object]' ) {
+                    extended[prop] = extend( true, extended[prop], obj[prop] );
+                } else {
+                    extended[prop] = obj[prop];
+                }
+            }
+        }
+    };
+
+    for ( ; i < length; i++ ) {
+        var obj = arguments[i];
+        merge(obj);
+    }
+
+    return extended;
+
+};
+
+window.hasClass = function(elem, className) {
+    return elem.className.split(' ').indexOf(className) > -1;
+}
+
+Element.prototype.parents = function(selector) {
+    var elements = [];
+    var elem = this;
+    var isHaveSelector = selector !== undefined;
+
+    while ((elem = elem.parentElement) !== null) {
+        if (elem.nodeType !== Node.ELEMENT_NODE) {
+            continue;
+        }
+
+        if (!isHaveSelector || elem.matches(selector)) {
+            elements.push(elem);
+        }
+    }
+
+    return elements;
+};
+
+String.prototype.capitalizeFirstLetter = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+}
+
+

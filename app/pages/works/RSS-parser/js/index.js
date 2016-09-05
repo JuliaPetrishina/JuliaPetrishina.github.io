5aad20d5f17ec3ba49a6eb26745993085556971a
juliapetrishina <juliapetrishina@sliceart.com.ui>
2016-09-05 20:00:24 +0300

    RSS-parser in progress

--- app/pages/works/RSS-parser/js/index.js
+++ app/pages/works/RSS-parser/js/index.js
@@ -6,16 +6,21 @@ welcome('home');
 var $ = require("jquery");
 
 //feed to parse
-var feed = "../news.xml";
-$.ajax(feed, {
+var FEED = "http://k.img.com.ua/rss/ua/news.xml";
+$.ajax(FEED, {
     accepts: {
         xml: "application/rss+xml"
     },
     type: 'GET',
     crossDomain: true,
     dataType: "xml",
-    success: showDescription
+    success: init
 });
+
+function init(data) {
+    showDescription(data);
+    getCategory(data);
+}
 
 function showDescription (data) {
     $(data).find("item").each(function () {
@@ -25,31 +30,40 @@ function showDescription (data) {
                 '<div class="image">' + el.find('image').text() + '</div>' +
                 '<div class="title">' + el.find('title').text() + '</div>' +
             '</div>');
-
-        getCategory(data);
-    });
+    });    
 }
 
 function getCategory (data) {
     $(data).find("item").each(function () {
         var el = $(this);
-        var category = el.find("category");
+        var categoryHasSportNews = el.find("category[domain*='/sport/']");
+        var categoryHasWorldNews = el.find("category[domain*='/world/']");
+        var category = el.find("category[domain*='/world/']");
+       
         var domain = category.attr('domain');
-        var sportNews = domain.indexOf('sport');
-        var worldNews = domain.indexOf('world');
+        var domainSport = categoryHasSportNews.attr('domain');
+        var domainWorld = categoryHasWorldNews.attr('domain');
 
         $('.select').change(function () {
             var optionSelected = $(this).find("option:selected");
             var valueSelected  = optionSelected.val();
-
-            if(valueSelected === 'sport') {
-                var sportEl = el.has(sportNews);
-
+            
+            if(valueSelected === 'sport' && domainSport !== undefined) {
+                console.log(domainSport);
+                console.log(el.find('category[domain="' + domainSport + '"]'));
             }
-            else if (valueSelected === 'world'){
-
+            else if (valueSelected === 'world' && domainWorld !== undefined){
+                var currentEl = el.find('category[domain="' + domainWorld + '"]');
+                var currentItem = currentEl.closest('item');
+                $('.col-xs-10').replaceWith(
+                    '<div class="col-xs-4">' +
+                        '<div class="image">' + currentItem.find('image').text() + 'lhjfybg;ilaefbglahfbvgldsujhflfyhv' + '</div>' +
+                        '<div class="title">' + currentItem.find('title').text() + '</div>' +
+                    '</div>');
+                console.log(currentItem);
             }
         });
+
     });
 }
 
--- app/pages/works/RSS-parser/webpack.config.js
+++ app/pages/works/RSS-parser/webpack.config.js
@@ -35,5 +35,5 @@ module.exports = {
   //    },
   //  ],
   //},
-  watch: true,
+  watch: true
 };
